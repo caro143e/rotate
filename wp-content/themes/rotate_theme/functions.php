b20c150cc8c8ef128e12138dc310fcb8348ed991
Helena Nielsen <hele168f@stud.kea.dk>
2023-11-27 19:22:12 +0100

    custom css

--- wp-content/themes/rotate_theme/functions.php
+++ wp-content/themes/rotate_theme/functions.php
@@ -1,6 +1,11 @@
 <?php 
 add_action('wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles(){
-    wp_enqueue_style( 'parent-style', get_template_directory_uri(). '/style.css')
+
+function enqueue_parent_styles() {
+    // Enqueue the parent theme style.css
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+
+    // Enqueue your global.css file
+    wp_enqueue_style('global-style', get_template_directory_uri() . '/global.css', array('parent-style'));
 }
 ?>
