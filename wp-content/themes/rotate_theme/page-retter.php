f4104da072991e70e574e8d03618c82a5c58fa63
Helena Nielsen <hele168f@stud.kea.dk>
2023-11-30 17:56:12 +0100

    .

--- wp-content/themes/rotate_theme/page-retter.php
+++ wp-content/themes/rotate_theme/page-retter.php
@@ -13,32 +13,21 @@ get_header();
         </template>
 <section id="primary" class="content-area">
     <main id="main" class="site-main">
-        <section class="produktcontainer"></section>
+        <section class="retcontainer"></section>
     </main>
 
     <script>
-        let retter;
+        let ret;
         const dbUrl = "https://loststudios.dk/kea/rotate/wp-json/wp/v2/ret?per_page=100";
 
         async function getJson() {
-            const data = await fetch(dbUrl);
-            retter = await data.json();
-            console.log(retter);
+            let response = await fetch(url);
+            retter = await respone.json();
+            console.log(sundays);
             visRetter();
         }
 
-        function visRetter() {
-            let temp = document.querySelector("template");
-            let container = document.querySelector(".produktcontainer")retter.forEach(ret => {
-                let klon = temp.cloneNode(true).content;
-                klon.querySelector("h2").textContent = ret.title.rendered;
-                klon.querySelector("img").src = ret.produktbillede.guid;
-                klon.querySelector(".pris").textContent = ret.pris;
-                klon.querySelector(".farve").textContent = ret.farve;
-                //klon.querySelector("article").addEventListener("click", ()=> {location.href = "restdb-single.html?id"+sunday._id;})
-                container.appendChild(klon);
-            })
-        }
+    
 
         getJson();
 
