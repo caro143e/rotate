ae00b9f2a18c2bcd5d5f4a1e604cc2c4d40f013b
Helena Nielsen <hele168f@stud.kea.dk>
2023-11-30 17:33:42 +0100

    .

--- wp-content/themes/rotate_theme/page-retter.php
+++ wp-content/themes/rotate_theme/page-retter.php
@@ -32,7 +32,7 @@ get_header();
             let container = document.querySelector(".produktcontainer")retter.forEach(ret => {
                 let klon = temp.cloneNode(true).content;
                 klon.querySelector("h2").textContent = ret.title.rendered;
-                klon.querySelector("img").src = produktbillede.guid;
+                klon.querySelector("img").src = ret.produktbillede.guid;
                 klon.querySelector(".pris").textContent = ret.pris;
                 klon.querySelector(".farve").textContent = ret.farve;
                 //klon.querySelector("article").addEventListener("click", ()=> {location.href = "restdb-single.html?id"+sunday._id;})
