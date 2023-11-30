2f22bdb08e8aa909a587c0d2f09b9e6be605e569
Helena Nielsen <hele168f@stud.kea.dk>
2023-11-30 18:07:52 +0100

    .

--- wp-content/themes/rotate_theme/page-retter.php
+++ wp-content/themes/rotate_theme/page-retter.php
@@ -2,29 +2,45 @@
 get_header();
 ?>
 <template>
-            <article>
-                <img src="" alt="">
-                <div>
-                    <h2></h2>
-                    <p>pris</p>
-                    <p>farve</p>
-                </div>
-            </article>
-        </template>
+    <article>
+        <img src="" alt="">
+            <div>
+                <h2></h2>
+                <p class="pris"></p>
+                <p class="farve"></p>
+            </div>
+    </article>
+</template>
+
 <section id="primary" class="content-area">
     <main id="main" class="site-main">
         <section class="retcontainer"></section>
     </main>
 
     <script>
-        let ret;
+        let retter;
         const dbUrl = "https://loststudios.dk/kea/rotate/wp-json/wp/v2/ret?per_page=100";
 
         async function getJson() {
-            let response = await fetch(url);
-            retter = await respone.json();
-            console.log(sundays);
+            const data = await fetch(dbUrl);
+            retter = await data.json();
+            console.log(retter);
             visRetter();
+        }
+
+        function visRetter() {
+            let temp = document.querySelector("template");
+            let container = document.querySelector(".retcontainer")
+            retter.forEach(ret => {
+                let klon = temp.cloneNode(true).content;
+                klon.querySelector("h2").textContent = ret.title.rendered;
+                klon.querySelector("img").src = ret.guid.rendered;
+                klon.querySelector("pris").textContent = ret.pris;
+                klon.querySelector("farve").textContent = ret.farve;
+
+                container.appendChild(klon);
+
+            })
         }
 
     
