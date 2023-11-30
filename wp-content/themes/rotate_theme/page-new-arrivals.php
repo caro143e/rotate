af7270a072a74ca4469805045b8ab8071a9da23a
Helena Nielsen <hele168f@stud.kea.dk>
2023-11-30 19:23:31 +0100

    .

--- wp-content/themes/rotate_theme/page-new-arrivals.php
+++ wp-content/themes/rotate_theme/page-new-arrivals.php
@@ -13,28 +13,28 @@ get_header();
 
 <section id="primary" class="content-area">
     <main id="main" class="site-main">
-        <section class="retcontainer"></section>
+        <section class="newcontainer"></section>
     </main>
 
     <script>
-        let retter;
-        const dbUrl = "https://loststudios.dk/kea/rotate/wp-json/wp/v2/ret?per_page=100";
+        let new;
+        const dbUrl = "https://loststudios.dk/kea/rotate/wp-json/wp/v2/new?per_page=100";
 
         async function getJson() {
             const data = await fetch(dbUrl);
-            retter = await data.json();
-            console.log(retter);
-            visRetter();
+            new = await data.json();
+            console.log(new);
+            visNew();
         }
 
-        function visRetter() {
+        function visNew() {
             let temp = document.querySelector("template");
-            let container = document.querySelector(".retcontainer")
-            retter.forEach(ret => {
+            let container = document.querySelector(".newcontainer")
+            new.forEach(new => {
                 let klon = temp.cloneNode(true).content;
-                klon.querySelector(".title").textContent = ret.title.rendered;
-                klon.querySelector("img").src = ret.produktbillede.guid;
-                klon.querySelector(".pris").textContent = ret.pris;
+                klon.querySelector(".title").textContent = new.title.rendered;
+                klon.querySelector("img").src = new.produktbillede.guid;
+                klon.querySelector(".pris").textContent = new.pris;
 
                 container.appendChild(klon);
 
