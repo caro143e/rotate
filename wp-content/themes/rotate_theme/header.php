c4e455b7c09923dbaa8e2a9ad7d89939b34c3ef8
Helena Nielsen <hele168f@stud.kea.dk>
2023-12-02 16:30:23 +0100

    header

--- wp-content/themes/rotate_theme/header.php
+++ wp-content/themes/rotate_theme/header.php
@@ -58,7 +58,7 @@ if ( apply_filters( 'astra_header_profile_gmpg_link', true ) ) {
             <ul class="nav-menu">
                 <li><a href="https://loststudios.dk/kea/rotate/new-arrivals/">New Arrivals</a></li>
                 <li><a href="https://loststudios.dk/kea/rotate/shop/">Shop All</a></li>
-                <li><a href="https://loststudios.dk/kea/rotate/rotate-party/"></a>ROTATE PARTY</li>
+                <li><a href="https://loststudios.dk/kea/rotate/rotate-party/">ROTATE PARTY</a></li>
                 <li><a href="https://loststudios.dk/kea/rotate/rotate-sunday/">Rotate Sunday</a></li>
                 <li><a href="https://loststudios.dk/kea/rotate/rotate-world/">Rotate World</a></li>
             </ul>
